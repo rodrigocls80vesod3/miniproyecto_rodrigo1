2ee955ad68130684dc14e715ee8d04cec0690e0b
RODRIGO  BARAHONA <84034531+rodrigocls80vesod3@users.noreply.github.com>
2021-06-16 14:24:41 -0500

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -1,1 +1,14 @@
-
+<?php
+namespace App\Models;
+use codeigniter\Model;
+class DatosModel extends Model{
+    public  function obtenerinformacion($data){
+      $gModel = $this->db->table('persona');
+      $gModel->where($data);
+      return $gModel->get()->getResultArray();
+      
+      
+    }
+  
+  public function insertar ($data){
+    $gModel = $this 
