1dc309fb470c3d9ffe1109c34862ec4347cf7163
RODRIGO  BARAHONA <84034531+rodrigocls80vesod3@users.noreply.github.com>
2021-06-17 12:17:07 -0500

    Update listado.php

--- codeingniter/app/Views/listado.php
+++ codeingniter/app/Views/listado.php
@@ -65,4 +65,28 @@ class="row">
               </div>
               </div>
               
-          
+            <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+   <script>
+              <script>
+
+let mensaje = '<?php echo $mensaje; ?>';
+if (mensaje == '0') {
+swal(':D', 'Registro insertado correctamente', 'success');
+}else if (mensaje == '1') {
+swal(':', 'No fue posible insertar registro correctamente', 'error');
+}else if (mensaje '2') {
+swal('D', 'Registro actualizado correctamente', 'success');
+Jelse if (mensaje == '3') {
+swal(':', 'No fue posible actualizar registro correctamente', 'error');
+Jelse if (mensaje '4') {
+swal('D', 'Registro eliminado correctamente', 'success');
+}else if(mensaje '5') {
+swal(':', 'No fue posible eliminar registro correctamente', 'error');
+}
+              
+              
+</script>
