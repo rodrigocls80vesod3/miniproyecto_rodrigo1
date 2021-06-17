d6fd4d22af779212b4c3fbbba354ea568cc30ce8
RODRIGO  BARAHONA <84034531+rodrigocls80vesod3@users.noreply.github.com>
2021-06-17 11:24:48 -0500

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -10,5 +10,44 @@ class DatosModel extends Model{
       
     }
   
-  public function insertar ($data){
-    $gModel = $this 
+  public function listartodo(){
+    $gModel = $this->db->query("SELECT + FROM persona");
+      return $gModel->getResult();
+      
+      
+  }
+    
+    
+    public function insertar($data){
+        $gModel = $this->db->table('persona');
+        $gModel->insert($data);
+        return $this->db->insertID();
+        
+        
+        
+    } 
+    
+    
+    
+    public function actualizar ($data,$id){
+        $gModel = $this->db->table('persona');
+        $gModel->set($data);
+        $gModel->where($id);
+        return $gModel->update();
+        
+        
+    }
+    
+    
+    
+    public function  eliminar($id){
+        $gModel = $this->db->table('persona');
+        $gModel = $this->where($id);
+        return $gModel->delete();
+        
+        
+    }
+    
+    
+    
+}
